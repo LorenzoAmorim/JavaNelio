9600ba7d6be186d612ef9638b6ac09a6ef67f557
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-05-20 01:21:20 -0300

    Iniciando estruturas repetitivas

---
+++ 03ESTRUTURA_REPETITIVA/main2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int x = sc.nextInt();
+        int soma = 0;
+        
+        while(x !=0){
+            soma = soma + x;
+            x = sc.nextInt();
+        }
+
+        System.out.println("A soma dos números digitados são: " + soma);
+
+        sc.close();
+    }
+}
