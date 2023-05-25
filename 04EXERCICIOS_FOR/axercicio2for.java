ead327c4cc52b2003a20ccd05ae453706e31a67d
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-05-24 22:23:43 -0300

    exercícios FOR inacabados

---
+++ 04EXERCICIOS_FOR/axercicio2for.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class axercicio2for {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        for(int X; )
+
+        //À TERMINAR
+
+        sc.close();
+    }
+}
---
+++ 04EXERCICIOS_FOR/exercicio1for.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class exercicio1for {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+            for(int i =1; i<=x; i++){
+                if(i % 2 != 0){
+                    System.out.println(i);
+                }
+            }
+
+
+        sc.close();
+    }
+}
