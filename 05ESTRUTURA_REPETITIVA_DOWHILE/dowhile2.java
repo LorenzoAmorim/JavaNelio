ff581ea8921cc00e29f84b916ba5bfadea6caed1
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-05-27 21:16:28 -0300

    Estruturas repetitivas DO-WHILE finalizadas, operadores bitwise vistos e iniciando funções para string

--- 05ESTRUTURA_REPETITIVA_DOWHILE/dowhile2.java
+++ 05ESTRUTURA_REPETITIVA_DOWHILE/dowhile2.java
@@ -5,9 +5,13 @@ public class dowhile2 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
+        //PROGRAMA COM O 'DO-WHILE'
+        /*AQUI O CHAR É CHAMADO FORA DOS COMANDOS POIS SE 
+        FOR CHAMADO DENTRO, TERÁ DE CHAMÁ-LO MAIS DE UMA VEZ, 
+        ASSIM ECONOMIZANDO LINHA*/
         char resp;
-        do {
+        do {/*FAÇA- LER A TEMPERATURA EM CELSIUS, INSERIR O VALOR DE 'C'
+            DEFINIR A FÓRMULA DE F,*/
             System.out.print("Digite a temperatura em Celsius: ");
             double C = sc.nextDouble();
             double F = (9*C/5) + 32;
---
+++ 07FUNCOES_PARA_STRING/funcaostring1.java
@@ -0,0 +1,11 @@
+public class funcaostring1 {
+    public static void main(String[] args) {
+         
+        String original = "abcde FGHIJ ABC abc DEFG   ";
+
+        String s1 = original.toLowerCase();
+        
+        System.out.println("Original: -" + original + "-");
+        System.out.println("Original: -" + s1 + "-");
+        }
+}
