ca4ed97d533885db9bae72579be6b3c0b68a2877
Lorenzo Amorim <lorenzoamorim@hotmail.com>
2023-06-07 16:42:25 -0300

    Finalizando o segundo exercício de métodos e classes

--- SEGUNDO_EXERCICIO_CLASSES_METODOS/course/src/application/Employee.java
+++ SEGUNDO_EXERCICIO_CLASSES_METODOS/course/src/application/Employee.java
@@ -17,6 +17,16 @@ public class Employee {
         data.GrossSalary = sc.nextDouble();
         System.out.print("Tax: ");
         data.Tax = sc.nextDouble();
+        System.out.println();
+        System.out.print("Employee: " + data.name + ", " + "$ " + data.NetSalary());
+        System.out.println("\n");
+        System.out.print("Wich percentage to increase salary? ");
+        /*Solicita ao usuário a porcentagem de aumento salarial desejada e a armazena na variável percentage. 
+        Em seguida, o método IncreaseSalary() do objeto data é chamado, passando a porcentagem como argumento.
+        Isso aumenta o salário bruto de acordo com a porcentagem fornecida.*/
+        double percentage = sc.nextDouble();
+        data.IncreaseSalary(percentage);
+        System.out.println("\nUpdated data: " + data.toString());
 
         sc.close();
     }
--- SEGUNDO_EXERCICIO_CLASSES_METODOS/course/src/entities/EmployeeData.java
+++ SEGUNDO_EXERCICIO_CLASSES_METODOS/course/src/entities/EmployeeData.java
@@ -4,12 +4,14 @@ public class EmployeeData {
     public String name;
     public double GrossSalary;
     public double Tax;
-    public double percentage;
 
     public double NetSalary(){
         return GrossSalary - Tax;
     }
     public void IncreaseSalary(double percentage){
-        
+        GrossSalary += GrossSalary * percentage/100.0;
+    }
+    public String toString(){
+        return name + ", " + "$ " + String.format("%.2f" , NetSalary());
     }
 }
